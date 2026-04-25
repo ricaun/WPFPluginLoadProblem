e4ee365ddb17922d6d372375f75eecd31e00bcf1
ricaun <ricaun@gmail.com>
2026-04-25 16:29:36 -0300

    Update Plugins to use the "http://helpers/wpf"

--- SecondPlugin/SecondPlugin.cs
+++ SecondPlugin/SecondPlugin.cs
@@ -32,9 +32,38 @@ namespace SecondPlugin
     </Style>
 </ResourceDictionary>
 ";
+            // The below XAML uses "http://helpers/wpf" to reference the Helpers assembly, which in this plugin is Version 2.0.0.0
+            var xaml2 = @"
+<ResourceDictionary
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+    xmlns:p=""http://helpers/wpf"">
+    <Style x:Key=""ButtonWithName"" TargetType=""{x:Type Button}"">
+        <Setter Property=""p:PropertiesVersion2.ButtonName"" Value=""Frank""/>
+    </Style>
+</ResourceDictionary>
+";
+            xaml = xaml2;
+            Console.WriteLine(xaml);
             var reader = new StringReader(xaml);
             var xml = XmlReader.Create(reader);
-            XamlReader.Load(xml);
+            var o = XamlReader.Load(xml);
+            if (o is System.Windows.ResourceDictionary rd)
+            {
+                var style = rd["ButtonWithName"] as System.Windows.Style;
+                Console.WriteLine($"Successfully loaded XAML with style: {style}");
+                // get setter property value
+                var setter = style.Setters[0] as System.Windows.Setter;
+                Console.WriteLine($"Successfully loaded XAML with setter: {setter}");
+
+                var property = setter.Property;
+                var value = setter.Value;
+                Console.WriteLine($"Successfully loaded XAML with setter property: {property} and value: {value}");
+                var assembly = property.OwnerType.Assembly;
+                Console.WriteLine($"Successfully loaded XAML with setter property assembly: {assembly}");
+                var context = System.Runtime.Loader.AssemblyLoadContext.GetLoadContext(assembly);
+                Console.WriteLine($"Successfully loaded XAML with setter property assembly load context: {context}");
+            }
         }
     }
 }
