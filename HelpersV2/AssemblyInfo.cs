09e7c1fdbf993095d2228e1ebc7097e5c17eef64
ricaun <ricaun@gmail.com>
2026-04-25 16:26:00 -0300

    Add `XmlnsPrefix`

---
+++ HelpersV2/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://helpers/wpf", "p")]
+[assembly: XmlnsDefinition("http://helpers/wpf", "Helpers")]
+// xmlns:p="http://helpers/wpf"
